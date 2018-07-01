54401e6a335d407038bfcd0a1319b3dd9ad302f8
ManishaVairamuthu <32608214+ManishaVairamuthu@users.noreply.github.com>
2018-07-01 10:29:09 +0530

    Update numdigi.java

--- numdigi.java
+++ numdigi.java
@@ -4,13 +4,20 @@ class numdigi
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        int n=Math.abs(sc.nextInt());
         int count=0;
+        if(n==0)
+        {
+            count=1;
+        }
+        else
+        {
         while(n>0)
         {
             int t=n%10;
             count++;
             n=n/10;
+        }
         }
         System.out.println(count);
     }
