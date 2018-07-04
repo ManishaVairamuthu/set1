33b35dad924be3f6d7324be6da01bdf709dccefa
ManishaVairamuthu <32608214+ManishaVairamuthu@users.noreply.github.com>
2018-07-04 21:36:46 +0530

    Create evennumbers.java

---
+++ evennumbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class evennumbers
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=sc.nextInt();
+        int out=0;
+        for(int i=n+1;i<n1;i++)
+        {
+         if(i%2==0)
+         System.out.print(i+" ");
+        }
+    }
+}
